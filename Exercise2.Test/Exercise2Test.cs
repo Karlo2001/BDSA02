8506b647bec7fa49b19c0fd8ac5046a23f211fa7
Karlo2001 <43517053+Karlo2001@users.noreply.github.com>
2021-09-22 13:33:48 +0200

    Exercise 2 3 4 (#1)

---
+++ Exercise2.Test/Exercise2Test.cs
@@ -0,0 +1,70 @@
+using System;
+using Xunit;
+using System.IO;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Exercise2.Test
+{
+    public class UnitTest1
+    {
+         // Retrieved from https://stackoverflow.com/a/62852186
+        private static bool IsPropertyReadOnly<T>(string PropertyName)
+        {
+            PropertyInfo info = typeof(T).GetMember(PropertyName)[0] as PropertyInfo;
+            return info.GetSetMethod() == null;
+        }
+
+        [Fact]
+        public void Status_Cant_Be_Changed()
+        {            
+            Assert.True(IsPropertyReadOnly<ImmutableStudent>("Status"));
+        }
+
+        [Fact]
+        public void ID_Cant_Be_Set_After_Created()
+        {            
+            Assert.True(IsPropertyReadOnly<ImmutableStudent>("Status"));
+        }
+
+        [Fact]
+        public void ToString_Returns_Complete_Object() 
+        {
+            // Arrange
+            var student = new ImmutableStudent(1, "Rasmus", "Lysstrøm", new DateTime(2021, 9, 16), new DateTime(2024, 7, 20), new DateTime(2024, 7, 20));
+
+            // Act
+            var actual = student.ToString();
+            string expected = (
+                $"Id={student.Id}, " +
+                $"Name={student.GivenName}, " + 
+                $"Surname={student.Surname}, " + 
+                $"Status={student.Status}, " +
+                $"StartDate={student.StartDate.ToString()}, " +
+                $"EndDate={student.EndDate.ToString()}, " +
+                $"GraduationDate={student.GraduationDate.ToString()}"
+            );
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("2021-9-16", "2024-7-20", "2024-7-20", Status.New)]
+        [InlineData("2019-9-16", "2022-7-20", "2022-7-20", Status.Active)]
+        [InlineData("2019-9-16", "2020-7-20", null,        Status.Dropout)]
+        [InlineData("2019-9-16", "2020-7-20", "2020-7-20", Status.Graduated)]
+        public void Status_Returns_Correctly(string start, string end, string? graduation, Status expected) 
+        {
+            var startDate = DateTime.Parse(start);
+            var endDate = DateTime.Parse(end);
+            DateTime? graduationDate = graduation != null ? DateTime.Parse(graduation) : null;
+
+            var student = new ImmutableStudent(1, "Name", "Surname", startDate, endDate, graduationDate);
+
+            var actual = student.Status;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Exercise2/Exercise2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Exercise2
+{
+    public enum Status {
+        New, 
+        Dropout, 
+        Active, 
+        Graduated
+    }
+    public record ImmutableStudent
+    {
+        public ImmutableStudent(int id, string givenName, string surname, DateTime startDate, DateTime endDate, DateTime? graduationDate) {
+            Id = id;
+            GivenName = givenName;
+            Surname = surname;
+            StartDate = startDate;
+            EndDate = endDate;
+            GraduationDate = graduationDate;
+        }
+        public int Id {get; init;} 
+        public string GivenName{get; init;}
+        public string Surname {get; init;}
+        public Status Status {
+            get{
+            if(GraduationDate == null) 
+            {
+                return Status.Dropout;
+            } 
+            else if(GraduationDate < DateTime.Now) 
+            {
+                return Status.Graduated;
+            } 
+            else if(DateTime.Now.AddMonths(-6) < StartDate)
+            {
+                return Status.New;
+            } 
+
+            return Status.Active;
+        }}
+        
+        public DateTime StartDate {get; init;}
+        public DateTime EndDate {get; init;}
+        public DateTime? GraduationDate {get; init;}
+
+        public override string ToString() {
+            return (
+                $"Id={Id}, " +
+                $"Name={GivenName}, " + 
+                $"Surname={Surname}, " + 
+                $"Status={Status.ToString()}, " +
+                $"StartDate={StartDate.ToString()}, " +
+                $"EndDate={EndDate.ToString()}, " +
+                $"GraduationDate={GraduationDate.ToString()}"
+            );
+    }
+}
+}
